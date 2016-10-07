2fd1d63efc7685de734fa2ea06be164a00103930
黄俊钦 <uestc_ccse@outlook.com>
2016-10-07 18:12:29 +0800

    增加图片预览和下载功能

--- pages/single/single.js
+++ pages/single/single.js
@@ -1,10 +1,14 @@
+var longTouch = false;
+
 Page({
 	data:{
 		img:{},
 		results:[],
 		video:null,
 		hidden:false,
-		display:"none"
+		display:"none",
+		previewImage:"none",
+		saveImage:true,
 	},
 	onLoad: function(options){
 		var time = options.time.replace(/-/g,"/");
@@ -44,5 +48,58 @@ Page({
 				that.update();
 			}
 		})
+	},
+	previewImg:function(){
+		this.setData({
+			previewImage:"block"
+		});
+	},
+	saveImg:function(){
+		longTouch = true;
+		this.setData({
+			saveImage:false
+		})
+	},
+	closePrev:function(){
+		if(longTouch){
+			longTouch = false;
+		}else{
+			this.setData({
+				previewImage:"none"
+			});
+		}
+	},
+	actionSheetChange:function(){
+		this.setData({
+			saveImage:!this.data.saveImage
+		})
+	},
+	downloadFile:function(e){
+		var imgUrl = e.currentTarget.dataset.imageHref;
+		var that = this;
+		wx.downloadFile({
+  			url: imgUrl,
+  			type: 'image',
+  			success:function(res){
+  				var tempFilePath = res.tempFilePath;
+    			console.log(tempFilePath);
+    			wx.saveFile({
+      				tempFilePath:tempFilePath,
+      				success:function(res){
+        				var savedFilePath = res.savedFilePath;
+        				console.log(savedFilePath);
+        				that.setData({
+        					saveImage:true
+        				});
+      				},
+      				fail:function(res){
+      					console.log(res);
+      				}
+    			});
+  			},
+  			fail:function(res){
+  				console.log(res);
+  			}
+		});
 	}
 });
